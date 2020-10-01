3f87f509d40a9ae5e3c845de28ac8c55d8d4cc34
Timo van Zijll Langhout <655426+Timovzl@users.noreply.github.com>
2020-10-01 12:03:45 +0200

    Added an Adapter to avoid exposing domain components.

--- Application/ApplicationServices/OrderShippingApplicationService.cs
+++ Application/ApplicationServices/OrderShippingApplicationService.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Architect.EntityFramework.AmbientDbContexts;
+using ReferenceArchitecture.Application.Adapters.Orders;
 using ReferenceArchitecture.Domain.Databases;
 using ReferenceArchitecture.Domain.Orders;
 
 namespace ReferenceArchitecture.Application.ApplicationServices
 {
+	/// <summary>
+	/// Provides use cases related to the shipping of <see cref="Order"/>s.
+	/// </summary>
 	public class OrderShippingApplicationService
 	{
 		private IDbContextProvider<IReferenceDatabase> DbContextProvider { get; }
@@ -35,20 +39,6 @@ namespace ReferenceArchitecture.Application.ApplicationServices
 			return order.Id;
 		}
 
-		public Task<string> GetOrderStatus(long orderId)
-		{
-			// #TODO: Fix bug in execution strategy override for MockDbContextProvider
-
-			return this.DbContextProvider.ExecuteInDbContextScopeAsync(async executionScope =>
-			{
-				var order = await this.OrderRepo.GetOrderById(orderId);
-
-				if (order is null) throw new KeyNotFoundException($"Order {orderId} does not exist.");
-
-				return order.ShippingStatus.ToString(); // #TODO: Add adapter, so that domain model is free to change
-			});
-		}
-
 		public Task ShipOrder(long orderId)
 		{
 			return this.DbContextProvider.ExecuteInDbContextScopeAsync(async executionScope =>
@@ -59,10 +49,26 @@ namespace ReferenceArchitecture.Application.ApplicationServices
 
 				order.Ship();
 
+				// EF with ChangeTracking does not require explicit updates, but the comment is left here for clarification
 				//await this.OrderRepo.Update(order);
 
 				await executionScope.DbContext.SaveChangesAsync();
 				executionScope.Complete();
+			});
+		}
+
+		public Task<string> GetOrderShippingResult(long orderId)
+		{
+			return this.DbContextProvider.ExecuteInDbContextScopeAsync(async executionScope =>
+			{
+				var order = await this.OrderRepo.GetOrderById(orderId);
+
+				if (order is null) throw new KeyNotFoundException($"Order {orderId} does not exist.");
+
+				// The exposed result becomes part of a contract
+				// Avoid leaking parts of the domain model, since being part of a contract would render them hard-to-change
+				var result = ShippingResultAdapter.GetContractValue(order.ShippingStatus.Result);
+				return result;
 			});
 		}
 	}
